10f386c32f5e81ad360cec48ba8c7946a50cec99
PurushottamGurjar <purushottamnitwarangal@gmail.com>
2025-07-02 15:13:46 +0530

    working on theme

--- src/assets/myassets.js
+++ src/assets/myassets.js
@@ -3,11 +3,13 @@ import menu_icon2 from "./menu_icon2.png";
 import cross_icon from "./cross_icon.png";
 import ESarthee_Logo from "./E-Sarthee_Logo.png"
 import rightarrow_icon from "./rightarrow_icon.svg"
+import blueMenu_Icon from "./blueMenu_Icon.svg"
 
 export const sartheeIcons={
     menu_icon,
     menu_icon2,
     cross_icon,
     rightarrow_icon,
-    ESarthee_Logo
+    ESarthee_Logo,
+    blueMenu_Icon
 }
--- src/components/E-Sarthee.jsx
+++ src/components/E-Sarthee.jsx
@@ -1,22 +1,22 @@
-import React, { useEffect ,useRef, useState} from 'react'
-import "./esarthee.css"
-import { io } from 'socket.io-client'
-import { sartheeIcons } from '../assets/myassets';
+import React, { useEffect, useRef, useState } from "react";
+import "./esarthee.css";
+import { io } from "socket.io-client";
+import { sartheeIcons } from "../assets/myassets";
 
-const socket=io("https://e-sarthee.onrender.com");
-
+const socket = io("https://e-sarthee.onrender.com");
 
 const ESarthee = () => {
-  const [isMenu, setIsMenu]=useState(false);
+  const [color, setcolor] = useState("#007BFF"); //green #008000 //blue #007BFF //amother green #0500ff
+  const [isMenu, setIsMenu] = useState(false);
   const map = useRef(null);
-  const markerRef = useRef(null); 
+  const markerRef = useRef(null);
   const marker1Ref = useRef(null);
 
-  useEffect(()=>{
-    map.current=L.map("map").setView([27.48,76.86],15);
-    L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png",{
-      maxZoom:20,
-      attribution:'&copy; OpenStreetMap | Purushottam Gurjar'
+  useEffect(() => {
+    map.current = L.map("map").setView([27.48, 76.86], 15);
+    L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      maxZoom: 20,
+      attribution: "&copy; OpenStreetMap | Purushottam Gurjar",
     }).addTo(map.current);
 
     socket.on("fetch-all-locations", (data) => {
@@ -24,86 +24,190 @@ const ESarthee = () => {
 
       if (markerRef.current) {
         markerRef.current.setLatLng([latitude, longitude]);
-        console.log("new location got")
+        console.log("new location got");
       } else {
         markerRef.current = L.marker([latitude, longitude], {
           title: "Van Location",
-        }).addTo(map.current)
+        })
+          .addTo(map.current)
           .bindPopup("Van Live Location")
           .openPopup();
       }
-    })
+    });
 
-    return ()=>{
+    return () => {
       map.current.remove();
     };
-
-  },[]);
-  
-
-
+  }, []);
 
   return (
     <div>
-        <div className="sarthee-heading">
-          <div className="sarthee-heading-first">E-Sarthee</div>
-          <div className="sarthee-heading-second">
-            <div className="nav-about  each-nav-item" onClick={()=>window.open("https://purushottam-gurjar.vercel.app",'_self')}>&lt;About Developer&gt;</div>
-            <div className="nav-connect each-nav-item" onClick={()=>window.open("https://www.linkedin.com/in/purushottam-gurjar/",'_self')} >Connect</div>
-            <div className="nav-connect each-nav-item" onClick={()=>window.open("https://github.com/PurushottamGurjar/E-Sarthee2.0",'_self')} >Contribute</div>
-            <div className="nav-contact each-nav-item" onClick={()=>window.open("https://contact-purushottam-gurjar.vercel.app",'_self')}>Contact</div>
-            
-            <div className="nav-install each-nav-item">Install App</div>
+      <div className="sarthee-heading">
+        <div className="sarthee-heading-first" style={{ color: color }}>
+          E-Sarthee
+        </div>
+        <div className="sarthee-heading-second">
+          <div
+            className="nav-about  each-nav-item"
+            onClick={() =>
+              window.open("https://purushottam-gurjar.vercel.app", "_self")
+            }
+          >
+            &lt;About Developer&gt;
           </div>
-          <div className="sarthee-menu">
-            {!isMenu && <img  className="sarthee-menu-icon" src={sartheeIcons.menu_icon2} alt="Menu" onClick={()=>setIsMenu(true)}/>}
-            {isMenu && <img  className="sarthee-menu-icon" src={sartheeIcons.cross_icon} alt="cross" onClick={()=>setIsMenu(false)} />}
+          <div
+            className="nav-connect each-nav-item"
+            onClick={() =>
+              window.open(
+                "https://www.linkedin.com/in/purushottam-gurjar/",
+                "_self"
+              )
+            }
+          >
+            Connect
+          </div>
+          <div
+            className="nav-connect each-nav-item"
+            onClick={() =>
+              window.open(
+                "https://github.com/PurushottamGurjar/E-Sarthee2.0",
+                "_self"
+              )
+            }
+          >
+            Contribute
+          </div>
+          <div
+            className="nav-contact each-nav-item"
+            onClick={() =>
+              window.open(
+                "https://contact-purushottam-gurjar.vercel.app",
+                "_self"
+              )
+            }
+          >
+            Contact
+          </div>
+
+          <div className="nav-install each-nav-item">Install App</div>
+        </div>
+        <div className="sarthee-menu">
+          {/* {!isMenu && <img  className="sarthee-menu-icon" src={sartheeIcons.menu_icon2} alt="Menu" onClick={()=>setIsMenu(true)}/>}
+            {isMenu && <img  className="sarthee-menu-icon" src={sartheeIcons.cross_icon} alt="cross" onClick={()=>setIsMenu(false)} />} */}
+          {!isMenu && (
+            <div>
+              <svg
+                width="40"
+                height="40"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                onClick={() => setIsMenu(!isMenu)}
+              >
+                <rect x="3" y="6" width="20" height="2.5" rx="1" fill={color} />
+                <rect x="3" y="12" width="20" height="2.5" rx="1" fill={color} />
+                <rect x="3" y="18" width="20" height="2.5" rx="1" fill={color} />
+              </svg>
+            </div>
+          )}
+          {isMenu && (
+            <div>
+              <svg
+                width="40"
+                height="40"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                onClick={()=>setIsMenu(!isMenu)}
+              >
+                <rect x="2" y="11" width="20" height="2.5" rx="1" fill="#007BFF" transform="rotate(45 12 12)" />
+                <rect x="2" y="11" width="20" height="2.5" rx="1" fill="#007BFF" transform="rotate(-45 12 12)" />
+                
+              </svg>
+            </div>
+          )}
+        </div>
+      </div>
+      <div id="map"></div>
+
+      {isMenu && (
+        <div className="sarthee-menu-content">
+          <div
+            className="sarthee-menu-each-item"
+            onClick={() =>
+              window.open("https://purushottam-gurjar.vercel.app", "_self")
+            }
+          >
+            <div className="">About Developer</div>
+            <img src={sartheeIcons.rightarrow_icon} alt="open" />
+          </div>
+
+          <div
+            className="sarthee-menu-each-item"
+            onClick={() =>
+              window.open(
+                "https://www.linkedin.com/in/purushottam-gurjar/",
+                "_self"
+              )
+            }
+          >
+            <div className="">Connect on LinkedIn</div>
+            <img src={sartheeIcons.rightarrow_icon} alt="open" />
+          </div>
+
+          <div
+            className="sarthee-menu-each-item"
+            onClick={() =>
+              window.open(
+                "https://github.com/PurushottamGurjar/E-Sarthee2.0",
+                "_self"
+              )
+            }
+          >
+            <div className="">Contribute - Github</div>
+            <img src={sartheeIcons.rightarrow_icon} alt="open" />
+          </div>
+
+          <div
+            className="sarthee-menu-each-item"
+            onClick={() =>
+              window.open(
+                "https://contact-purushottam-gurjar.vercel.app",
+                "_self"
+              )
+            }
+          >
+            <div className="">Contact</div>
+            <img src={sartheeIcons.rightarrow_icon} alt="open" />
+          </div>
+
+          <div className="sarthee-menu-each-item">
+            <div className="">Share Feeback</div>
+            <img src={sartheeIcons.rightarrow_icon} alt="open" />
+          </div>
+
+          <div
+            className="sarthee-menu-install"
+            onClick={() =>
+              window.open(
+                "https://drive.google.com/file/d/1lSNbT9ABIMsVvzVEMcvJyil7_FJzCp4A/view?usp=sharing",
+                "_self"
+              )
+            }
+            style={{ backgroundColor: color }}
+          >
+            <img
+              src={sartheeIcons.ESarthee_Logo}
+              alt="E-Sarthee"
+              className="E-Sarthee-logo"
+              
+            />
+            Install App now
           </div>
         </div>
-         <div id="map"></div>
-        
-          {isMenu && <div className="sarthee-menu-content">
-
-             <div className="sarthee-menu-each-item"
-              onClick={()=>window.open("https://purushottam-gurjar.vercel.app",'_self')}>
-              <div className="">About Developer</div>
-              <img src={sartheeIcons.rightarrow_icon} alt="open" />
-             </div>
-
-             <div className="sarthee-menu-each-item"
-             onClick={()=>window.open("https://www.linkedin.com/in/purushottam-gurjar/",'_self')}>
-              <div className="">Connect on LinkedIn</div>
-              <img src={sartheeIcons.rightarrow_icon} alt="open" />
-             </div>
-
-             <div className="sarthee-menu-each-item"
-             onClick={()=>window.open("https://github.com/PurushottamGurjar/E-Sarthee2.0",'_self')}>
-              <div className="">Contribute - Github</div>
-              <img src={sartheeIcons.rightarrow_icon} alt="open" />
-             </div>
-
-
-             <div className="sarthee-menu-each-item"
-             onClick={()=>window.open("https://contact-purushottam-gurjar.vercel.app",'_self')}>
-              <div className="">Contact</div>
-              <img src={sartheeIcons.rightarrow_icon} alt="open" />
-             </div>
-
-             <div className="sarthee-menu-each-item">
-              <div className="">Share Feeback</div>
-              <img src={sartheeIcons.rightarrow_icon} alt="open" />
-             </div>
-
-
-            <div className="sarthee-menu-install" onClick={()=>window.open("https://drive.google.com/file/d/1lSNbT9ABIMsVvzVEMcvJyil7_FJzCp4A/view?usp=sharing","_self")}>
-              <img src={sartheeIcons.ESarthee_Logo} alt="E-Sarthee" className="E-Sarthee-logo" />
-              Install App now</div>
-          
-          </div>}
-        
-        
+      )}
     </div>
-  )
-}
+  );
+};
 
 export default ESarthee;
